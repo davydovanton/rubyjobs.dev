ff8f3612371857e98ae5cd44d9d917e83f20c7c1
Anton Davydov <antondavydov.o@gmail.com>
2019-04-09 01:26:12 +0300

    Implement action for generate vacancy

--- lib/vacancies/operations/create.rb
+++ lib/vacancies/operations/create.rb
@@ -3,8 +3,66 @@
 module Vacancies
   module Operations
     class Create < ::Libs::Operation
-      def call(_payload = nil)
-        Success(Vacancy.new)
+      include Import[
+        vacancy_repo: 'repositories.vacancy',
+        contact_repo: 'repositories.contact'
+      ]
+
+      Dry::Validation.load_extensions(:monads)
+
+      VACANCY_VALIDATOR = Dry::Validation.JSON do
+        required(:position).filled(:str?)
+        required(:position_type).filled(:str?)
+
+        required(:details_raw).filled(:str?)
+
+        required(:location).filled(:str?)
+        required(:remote_available).filled(:bool?)
+
+        required(:salary_min).filled(:int?)
+        required(:salary_max).filled(:int?)
+        required(:salary_currency).filled(:str?)
+        required(:salary_unit).filled(:str?)
+
+        required(:archived_in_weeks).filled(:int?)
+      end
+
+      CONTACT_VALIDATOR = Dry::Validation.JSON do
+        required(:email).filled(:str?)
+        required(:full_name).filled(:str?)
+
+        optional(:company).maybe(:str?)
+        optional(:site).maybe(:str?)
+      end
+
+      def call(contact:, vacancy:)
+        company_payload = yield CONTACT_VALIDATOR.call(contact).to_either
+        vacancy_payload = yield VACANCY_VALIDATOR.call(vacancy).to_either
+
+        vacancy_payload[:details] = vacancy_payload[:details_raw]
+        vacancy_payload[:published] = false
+        vacancy_payload[:archived] = false
+        vacancy_payload[:archived_at] = calculate_archive_date(vacancy_payload[:archived_in_weeks])
+
+        persist_vacancy(company_payload, vacancy_payload)
+      end
+
+    private
+
+      # seconds * minutes * hours * days
+      WEEK = 60 * 60 * 24 * 7
+
+      def calculate_archive_date(archived_in_weeks)
+        (Time.now + archived_in_weeks * WEEK).to_date
+      end
+
+      def persist_vacancy(company_payload, vacancy_payload)
+        Try(Hanami::Model::UniqueConstraintViolationError) do
+          vacancy_repo.transaction do
+            contact = contact_repo.create(company_payload)
+            vacancy_repo.create(**vacancy_payload, contact_id: contact.id)
+          end
+        end.to_result
       end
     end
   end
--- spec/vacancies/operations/create_spec.rb
+++ spec/vacancies/operations/create_spec.rb
@@ -1,18 +1,89 @@
 # frozen_string_literal: true
 
 RSpec.describe Vacancies::Operations::Create, type: :operation do
-  let(:operation) { described_class.new }
+  let(:operation) do
+    described_class.new(vacancy_repo: vacancy_repo, contact_repo: contact_repo)
+  end
 
-  subject { operation.call }
+  let(:vacancy_repo) { instance_double('VacancyRepository', transaction: Vacancy.new, create: Vacancy.new) }
+  let(:contact_repo) { instance_double('ContactRepository', create: Vacancy.new) }
 
-  it { expect(subject).to be_success }
-  it { expect(subject.value!).to be_a(Vacancy) }
+  let(:vacancy_payload) do 
+    {
+      position: "ruby developer",
+      position_type: "full_time",
 
-  xcontext 'with real dependencies' do
+      details_raw: "test something here",
+
+      location: "moscow",
+      remote_available: true,
+
+      salary_min: 1000000,
+      salary_max: 222222222,
+      salary_currency: "rub",
+      salary_unit: "monthly",
+
+      archived_in_weeks: 2
+    }
+  end
+
+  let(:contact_payload) do
+    { email: "test@something.com", full_name: "First Name", company: "test", site: "" }
+  end
+
+  subject { operation.call(contact: contact_payload, vacancy: vacancy_payload) }
+
+  context 'when contact and vacancy data is valid' do
+    it { expect(subject).to be_success }
+    it { expect(subject.value!).to be_a(Vacancy) }
+
+  end
+
+  context 'when contact payload is invalid' do
+    let(:contact_payload) { {} }
+
+    it { expect(subject).to be_failure }
+  end
+
+  context 'when vacancy payload is invalid' do
+    let(:vacancy_payload) { {} }
+
+    it { expect(subject).to be_failure }
+  end
+
+  context 'with real dependencies' do
     let(:operation) { described_class.new }
+    let(:two_weeks_after) { (Time.now + 2 * (60 * 60 * 24 * 7)).to_date }
 
-    subject { operation.call }
+    subject { operation.call(contact: contact_payload, vacancy: vacancy_payload) }
 
     it { expect(subject).to be_success }
+
+    # HACK: I had troubles to mock `#create` call in transaction block. that's why I createad this hack
+    it 'setup specific values' do
+      vacancy = subject.value!
+
+      expect(vacancy.to_h[:archived_at].to_date).to eq(two_weeks_after)
+
+      expect(vacancy.to_h).to include(
+        position: "ruby developer",
+        position_type: "full_time",
+
+        details_raw: "test something here",
+        details: "test something here",
+        # details: "<p>test something here</p>",
+
+        location: "moscow",
+        remote_available: true,
+
+        salary_min: 1000000,
+        salary_max: 222222222,
+        salary_currency: "rub",
+        salary_unit: "monthly",
+
+        published: false,
+        archived: false,
+      )
+    end
   end
 end
