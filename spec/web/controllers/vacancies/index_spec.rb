387874ffc12a97e9523f58161d0b3095ff387f8e
Anton Davydov <antondavydov.o@gmail.com>
2019-04-09 15:13:52 +0300

    Create a action for displaying all published vacancies

--- spec/web/controllers/vacancies/index_spec.rb
+++ spec/web/controllers/vacancies/index_spec.rb
@@ -1,8 +1,24 @@
 RSpec.describe Web::Controllers::Vacancies::Index, type: :action do
-  let(:action) { described_class.new }
+  let(:action) { described_class.new(operation: operation) }
+  let(:operation) { ->(*) { Success([Vacancy.new(id: 123)]) } }
+
   let(:params) { Hash[] }
 
   subject { action.call(params) }
 
-  it { expect(subject).to be_success }
+  context 'when operation returns success value' do
+    it { expect(subject).to be_success }
+
+    it 'exposes list of vacancies' do
+      subject
+      expect(action.vacancies).to eq([Vacancy.new(id: 123)])
+    end
+  end
+
+  context 'with real dependencies' do
+    let(:action) { described_class.new }
+    let(:params) { Hash[] }
+
+    subject { action.call(params) }
+  end
 end
