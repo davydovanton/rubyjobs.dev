de09a66f4036245f279c47669e4098c6ff4c8915
Anton Davydov <antondavydov.o@gmail.com>
2019-04-10 00:25:05 +0300

    Create sticky footer

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -2,6 +2,6 @@ root to: 'vacancies#index'
 
 resources :vacancies, only: %i[new create show]
 
-get '/terms', to: 'static#terms'
-get '/about', to: 'static#about'
-get '/contacts', to: 'static#contacts'
+get '/terms', to: 'static#terms', as: :terms
+get '/about', to: 'static#about', as: :about
+get '/contacts', to: 'static#contacts', as: :contacts
