4e7cc50aa73f9c9cbceee4e424e8d6647bbe40b6
Anton Davydov <antondavydov.o@gmail.com>
2019-04-10 17:38:51 +0300

    Create better body for email response

--- apps/web/views/vacancies/show.rb
+++ apps/web/views/vacancies/show.rb
@@ -13,12 +13,21 @@ module Web
         end
 
         def respond_to_email_href
-          "mailto:#{vacancy.contact.email}?subject=Резюме на позицию \"#{vacancy.position}\"&body=TODO"
+          body = <<~HEREDOC
+            Привет!#{NEW_LINE_IN_EMAIL_BODY}
+            Увидел вакансию на rubyjobs.dev
+          HEREDOC
+
+          "mailto:#{vacancy.contact.email}?subject=Резюме на позицию \"#{vacancy.position}\"&body=#{body}"
         end
 
         def raw_body(body)
           raw(body || '')
         end
+
+      private
+
+        NEW_LINE_IN_EMAIL_BODY = '%0D%0A'
       end
     end
   end
