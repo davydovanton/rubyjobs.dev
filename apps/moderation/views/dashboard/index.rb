dc16b35992cc04868d6b514e4b6430856e99a080
Anton Davydov <antondavydov.o@gmail.com>
2019-04-11 14:48:16 +0300

    Create a layout for moderation dashboard

--- apps/moderation/views/dashboard/index.rb
+++ apps/moderation/views/dashboard/index.rb
@@ -3,6 +3,14 @@ module Moderation
     module Dashboard
       class Index
         include Moderation::View
+
+        def vacancy_details(vacancy)
+          raw_body(vacancy.details)
+        end
+
+        def raw_body(body)
+          raw(body || '')
+        end
       end
     end
   end
