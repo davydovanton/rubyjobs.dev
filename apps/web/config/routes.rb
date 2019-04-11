1268af1527be820c0b9bae81ce7ab5770274c7f0
Anton Davydov <antondavydov.o@gmail.com>
2019-04-11 19:17:44 +0300

    Add ssl again

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -5,3 +5,4 @@ resources :vacancies, only: %i[new create show]
 get '/terms', to: 'static#terms', as: :terms
 get '/about', to: 'static#about', as: :about
 get '/contacts', to: 'static#contacts', as: :contacts
+get '/.well-known/acme-challenge/26ZMu5hIuUyL_OOlI1A2tFx7eYQQtmu95Wk0XqnN_f0', to: ->(env) { [200, {}, ['26ZMu5hIuUyL_OOlI1A2tFx7eYQQtmu95Wk0XqnN_f0.Pykxyeexf4KnPKQPSOt5yVGIhi0eu0Jot6rWiqmKo_8!']] }
