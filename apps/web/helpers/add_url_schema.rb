6af73ad46c58a2c68fa6d019abd7c74ba167c82c
Anton Davydov <antondavydov.o@gmail.com>
2019-04-12 22:09:01 +0300

    Fix rubocop issues

--- apps/web/helpers/add_url_schema.rb
+++ apps/web/helpers/add_url_schema.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 module Web
   module Helpers
     module AddUrlSchema
       private
-      HTTP_SCHEMA_REGEXP = %r[\Ahttps?://]
+
+      HTTP_SCHEMA_REGEXP = %r{\Ahttps?://}.freeze
 
       def add_url_schema(string)
         string[HTTP_SCHEMA_REGEXP] ? string : "http://#{string}"
