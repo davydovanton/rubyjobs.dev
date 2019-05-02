9641ea4eab185c963039ab88631e7bbbe2c35591
Anton Davydov <antondavydov.o@gmail.com>
2019-05-02 12:01:43 +0300

    Create spec file for vacancy mapper

---
+++ spec/vacancies/mappers/vacancy_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+RSpec.describe Vacancies::Mappers::Vacancy, type: :mapper do
+  subject { mapper.call(contact: contact_payload, **vacancy_payload) }
+
+  let(:mapper) { described_class.new }
+
+  let(:vacancy_payload) do
+    {
+      position: 'ruby developer',
+      position_type: 'full_time',
+
+      details_raw: 'test something here',
+
+      location: 'moscow',
+      remote_available: '1',
+
+      salary: {
+        min: '1_000_000',
+        max: '222_222_222',
+        currency: 'rub',
+        unit: 'monthly'
+      },
+
+      archived_in_weeks: '2'
+    }
+  end
+
+  let(:contact_payload) do
+    { email: 'test@something.com', full_name: 'First Name', company: 'test', site: '' }
+  end
+
+  it 'maps data to right hash' do
+    expect(subject).to eq(
+      vacancy: {
+        position: 'ruby developer',
+        position_type: 'full_time',
+
+        details_raw: 'test something here',
+
+        location: 'moscow',
+        remote_available: true,
+
+        salary_min: 1_000_000,
+        salary_max: 222_222_222,
+        salary_currency: 'rub',
+        salary_unit: 'monthly',
+
+        archived_in_weeks: 2
+      },
+
+      contact: contact_payload
+    )
+  end
+end
