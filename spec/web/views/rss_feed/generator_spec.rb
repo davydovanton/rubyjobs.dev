4a744e094a0367161f840bad92b1d108e9f1d208
Alexandr Borisov <ab@cifronomika.ru>
2019-05-10 23:02:44 +0300

    Refactor rss generator spec

--- spec/web/views/rss_feed/generator_spec.rb
+++ spec/web/views/rss_feed/generator_spec.rb
@@ -17,10 +17,10 @@ RSpec.describe Web::Views::RssFeed::Generator, type: :view do
   it { expect(subject.date).to eq time }
 
   it { expect(subject.entries.size).to eq 1 }
-  it { expect(subject.entries[0].id.content).to eq 'https://rubyjobs.dev/vacancies/' }
-  it { expect(subject.entries[0].link.href).to eq 'https://rubyjobs.dev/vacancies/' }
-  it { expect(subject.entries[0].summary.content).to eq '<p>something here</p>' }
-  it { expect(subject.entries[0].title.content).to eq 'Senior mecha pilot' }
+  it { expect(subject.entries[0].id.content).to eq "https://rubyjobs.dev/vacancies/#{vacancy.id}" }
+  it { expect(subject.entries[0].link.href).to eq "https://rubyjobs.dev/vacancies/#{vacancy.id}" }
+  it { expect(subject.entries[0].summary.content).to eq vacancy.details }
+  it { expect(subject.entries[0].title.content).to eq vacancy.position }
   it { expect(subject.entries[0].updated.content).to eq time }
   it { expect(subject.entries[0].date).to eq time }
 end
