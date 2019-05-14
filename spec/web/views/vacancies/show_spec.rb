39fc8ae2aa73464255e3a9e7529ef8c4c0c90c26
Anton Davydov <antondavydov.o@gmail.com>
2019-05-14 14:53:45 +0200

    Create a simple test for checking render of vacancy show page

--- spec/web/views/vacancies/show_spec.rb
+++ spec/web/views/vacancies/show_spec.rb
@@ -1,10 +1,20 @@
 # frozen_string_literal: true
 
 RSpec.describe Web::Views::Vacancies::Show, type: :view do
-  let(:exposures) { Hash[format: :html] }
+  let(:exposures) { { format: :html, vacancy: vacancy_aggregate, flash: {} } }
   let(:template)  { Hanami::View::Template.new('apps/web/templates/vacancies/show.html.slim') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
+
+  let(:vacancy_aggregate) do
+    Vacancy.new(
+      contact: Contact.new(created_at: Time.now),
+      position: 'Ruby vacancy',
+      created_at: Time.now
+    )
+  end
+
+  it { expect(rendered).to match('Ruby vacancy') }
 
   it 'exposes #format' do
     expect(view.format).to eq exposures.fetch(:format)
