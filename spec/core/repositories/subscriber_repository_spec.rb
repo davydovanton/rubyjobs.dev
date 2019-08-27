0d4420b2b6a9fa6fd46a60eb85b0ed1bf6529adc
Anton Davydov <antondavydov.o@gmail.com>
2019-08-27 19:11:00 +0100

    Create a method for finding subscriber by email

--- spec/core/repositories/subscriber_repository_spec.rb
+++ spec/core/repositories/subscriber_repository_spec.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
 RSpec.describe SubscriberRepository, type: :repository do
-  # place your tests here
+  let(:repo) { described_class.new }
+
+  describe '#find_by_email' do
+    let(:email) { 'anton@test.com' }
+
+    subject { repo.find_by_email(email) }
+
+    context 'when subscriber with email exist' do
+      before { repo.create(email: email) }
+      
+      it { expect(subject).to be_a(Subscriber) }
+      it { expect(subject.email).to eq(email) }
+    end
+
+    context 'when subscriber with email does not exist' do
+      it { expect(subject).to be nil }
+    end
+  end
 end
