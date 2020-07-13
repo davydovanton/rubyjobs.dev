dc10dcc79c00d2663dd2b36ab00711cfaa667334
Simon <semenb1999@gmail.com>
2020-07-13 21:57:38 +0300

    New default fitures to the filter was added

--- apps/web/mappers/search_options.rb
+++ apps/web/mappers/search_options.rb
@@ -7,14 +7,22 @@ module Web
         {
           position_type: params[:position_type],
           location: params[:location],
-          salary_currency: params[:salary_currency],
           salary: to_int(params[:salary]),
+          salary_currency: check_currency(params[:salary_currency], params[:salary]),
           remote: to_bool(params[:remote]),
           text: params[:text]
         }
       end
 
       private
+
+      def check_currency(value, salary)
+        if value.nil? && !salary.nil?
+          'rub'
+        else
+          value
+        end
+      end
 
       def to_bool(value)
         case value
