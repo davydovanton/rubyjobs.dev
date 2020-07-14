247da0eea84c0a104867adbc9a82afeaba0d3072
Simon <semenb1999@gmail.com>
2020-07-14 17:06:59 +0300

    Fix tests

--- spec/web/mappers/search_options_spec.rb
+++ spec/web/mappers/search_options_spec.rb
@@ -27,24 +27,24 @@ RSpec.describe Web::Mappers::SearchOptions, type: :mapper do
     it { expect(subject[:salary_currency]).to eq nil }
   end
 
-  context 'when salary is string equaled "545"' do
+  context 'when salary is string equaled "545" and salary_currency is missing' do
     let(:search_options_hash) { { remote: nil, position_type: nil, location: nil, salary: '545' } }
 
     it { expect(subject[:remote]).to eq nil }
     it { expect(subject[:position_type]).to eq nil }
     it { expect(subject[:location]).to eq nil }
     it { expect(subject[:salary]).to eq 545 }
-    it { expect(subject[:salary_currency]).to eq nil }
+    it { expect(subject[:salary_currency]).to eq 'rub' }
   end
 
-  context 'when salary_currency is string equaled "rub"' do
-    let(:search_options_hash) { { remote: nil, position_type: nil, location: nil, salary: nil, salary_currency: 'rub' } }
+  context 'when salary_currency is string equaled "usd"' do
+    let(:search_options_hash) { { remote: nil, position_type: nil, location: nil, salary: nil, salary_currency: 'usd' } }
 
     it { expect(subject[:remote]).to eq nil }
     it { expect(subject[:position_type]).to eq nil }
     it { expect(subject[:location]).to eq nil }
     it { expect(subject[:salary]).to eq nil }
-    it { expect(subject[:salary_currency]).to eq 'rub' }
+    it { expect(subject[:salary_currency]).to eq 'usd' }
   end
 
   context 'when option is invalid' do
