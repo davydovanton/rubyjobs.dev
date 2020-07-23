02047871d2fe2f6a61af15b18652f75b554b49ef
Simon <semenb1999@gmail.com>
2020-07-23 22:26:45 +0300

    Добавлена возможность просмотреть добавленные интервью компании (временный вид)

--- apps/web/controllers/interviews/index.rb
+++ apps/web/controllers/interviews/index.rb
@@ -10,15 +10,15 @@ module Web
                     interview_operation: 'interviews.operations.list'
                 ]
 
-        expose :company, :interviews
+        expose :interviews, :company
 
         def call(params)
-          result = operation.call(id: params[:id])
-
-          case result
+          result = interview_operation.call(company_id: params[:company_id].to_i)
+          company_result = operation.call(id: params[:company_id].to_i)
+          case result && company_result
           when Success
-            @company = result.value!
-            @reviews = interview_operation.call(company_id: @company.id).value_or([])
+            @interviews = result.value_or([])
+            @company = company_result.value!
           when Failure
             redirect_to routes.companies_path
           end
