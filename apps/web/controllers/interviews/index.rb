80eceadbf6d279d9500326b8790491184e2356dc
Simon <semenb1999@gmail.com>
2020-07-30 21:27:47 +0300

    Исправлены замечания по миграциям и по контроллеру interviews/index

--- apps/web/controllers/interviews/index.rb
+++ apps/web/controllers/interviews/index.rb
@@ -15,10 +15,10 @@ module Web
         def call(params)
           result = interview_operation.call(company_id: params[:company_id].to_i)
           company_result = operation.call(id: params[:company_id].to_i)
-          case result && company_result
+          case company_result
           when Success
-            @interviews = result.value_or([])
             @company = company_result.value!
+            @interviews = result.value_or([])
           when Failure
             redirect_to routes.companies_path
           end
--- db/migrations/20200722145224_add_interview_ratings_to_companies.rb
+++ db/migrations/20200722145224_add_interview_ratings_to_companies.rb
@@ -4,7 +4,7 @@ Hanami::Model.migration do
   change do
     alter_table :companies do
       add_column :interview_rating_total, Float, null: false, default: 0.0
-      add_column :interview_ratings, 'jsonb', default: '{}', null: false
+      add_column :interviews, 'jsonb', default: '{}', null: false
     end
   end
 end
